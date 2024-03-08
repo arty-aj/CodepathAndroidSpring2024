43243574fbb46113a676935b650f18398af4693e
arty-aj <88049900+arty-aj@users.noreply.github.com>
2024-03-08 16:26:55 -0600

    Problem in EmailAdapter

--- app/src/main/java/com/example/codepathandroidspring2024/EmailAdapter.kt
+++ app/src/main/java/com/example/codepathandroidspring2024/EmailAdapter.kt
@@ -6,16 +6,15 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class EmailAdapter : RecyclerView.Adapter<EmailAdapter.ViewHolder>(){
+class EmailAdapter(private val emails: List<Email>) : RecyclerView.Adapter<EmailAdapter.ViewHolder>(){
     // Provide a direct reference to each of the views within a data item
     // Used to cache the views within the item layout for fast access
-    val emails = EmailFetcher.getEmails()
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         //Create member variables for any view that will be set
         //as you render a row.
-        lateinit var senderTextView : TextView
-        lateinit var titleTextView: TextView
-        lateinit var summaryTextView: TextView
+        val senderTextView : TextView
+        val titleTextView: TextView
+        val summaryTextView: TextView
 
         //We also create a constructor that accepts the entire item row
         //and does the view lookups to find each sub-view
