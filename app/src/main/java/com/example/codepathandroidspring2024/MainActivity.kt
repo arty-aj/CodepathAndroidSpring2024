be6dbf50de614182e5ffa8550191a78078b693d8
arty-aj <88049900+arty-aj@users.noreply.github.com>
2024-03-16 12:35:30 -0500

    Reverted back to being an empty app

--- app/src/main/java/com/example/codepathandroidspring2024/MainActivity.kt
+++ app/src/main/java/com/example/codepathandroidspring2024/MainActivity.kt
@@ -11,31 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getMovies()
-    }
-
-    private fun getMovies(){
-        val client = AsyncHttpClient()
-        val params = RequestParams()
-        //params["limit"] = "5"
-        //params["page"] = "0"
-        client["https://api.themoviedb.org/3/movie/now_playing?&api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed", params, object :
-            TextHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Headers, response: String) {
-                // called when response HTTP status is "200 OK"
-                println(response)
-            }
-
-            override fun onFailure(
-                statusCode: Int,
-                headers: Headers?,
-                errorResponse: String,
-                t: Throwable?
-            ) {
-                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                println(errorResponse)
-            }
-        }]
     }
 
 }
--- app/src/main/java/com/example/codepathandroidspring2024/models/Dates.kt
+++
@@ -1,6 +0,0 @@
-package com.example.codepathandroidspring2024.models
-
-data class Dates(
-    val maximum: String,
-    val minimum: String
-)
